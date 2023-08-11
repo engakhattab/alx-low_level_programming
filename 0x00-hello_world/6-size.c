3e0387b093b4b7b79362bcb79ba610b54f6886a4
engakhattab <engabdelrahmankhattab@gmail.com>
2023-08-10 18:45:36 -0700

    66

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
-
 /**
- * main - Entry
- *
- * Description: using sizeof to print the sixe of various types
- *
- * Return: Always 0 (Success)
-*/
+ * * main - Entry point
+ * *
+ * * Return: Always 0 (Success)
+ * */
 
 int main(void)
 {
-	printf("Size of a char: %u byte(s)/n", sizeof(char));
-	printf("Size of an int: %u byte(s)/n", sizeof(int));
-	printf("Size of a long int: %u byte(s)/n", sizeof(long int));
-	printf("Size of a long long int: %u byte(s)/n", sizeof(long long int));
-	printf("Size of a float: %u byte(s)/n", sizeof(float));
+	char forchar;
+	int forint;
+	long forlong;
+	long long forlonglong;
+	float forfloat;
+	printf("Size of a char: %lu byte(s)\n", sizeof(forchar));
+	printf("Size of an int: %lu byte(s)\n", sizeof(forint));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(forlong));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(forlonglong));
+	printf("Size of a float: %lu byte(s)\n", sizeof(forfloat));
 	return (0);
 }
