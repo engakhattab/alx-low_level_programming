128bf0f18afa0c54afa68dad605060d20010694a
engakhattab <engabdelrahmankhattab@gmail.com>
2023-08-17 18:52:19 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+include "main.h"
+
+/**
+ * _isupper - If is uppercase return 1, if not return 0
+ *
+ * @c: this is the entry
+ *
+ * Return: 0
+ *
+**/
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+}
+	return (0);
+}
