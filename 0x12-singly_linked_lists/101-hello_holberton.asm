ee4588a4719faca6ae0da41ea4146adebbdcd760
engakhattab <engabdelrahmankhattabgmail.com>
2023-09-21 23:57:53 +0000

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,12 +1,14 @@
-ection		.text
-	extern	printf
-	global	main
-
+ection   .text
+	global main
 main:
-	mov		edi, msg
-	mov		eax, 0
-	call	printf
+	mov       rax, 1
+	mov       rdi, 1
+	mov       rsi, msg
+	mov       rdx, 17
+	syscall
+	mov       rax, 60
+	xor       rdi, rdi
+	syscall
 
-
-section		.data
-	msg db 'Hello, Holberton', 0xa, 0
+	section   .data
+msg:	db        "Hello, Holberton", 10
